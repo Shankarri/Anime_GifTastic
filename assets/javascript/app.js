6479fb674ec7c2a69052b9bae187a4c528eb9d2c
Shanki <shanki.ranju@gmail.com>
2019-02-06 15:15:28 -0600

    updated file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,31 +2,61 @@
 $(document).ready(function () {
 
   var topics = ["Fairy Tail", "Inuyasha", "Death Note", "Fushigi Yugi", "Yu Yu Hakusho", "Fate Zero",
-    "Hunter X Hunter", "Bleach", "Full Metal Alchemist", "Fate Zero", "Sword Art Online", "Nanatsu no Taizai"];
+    "Hunter X Hunter", "Bleach", "Full Metal Alchemist", "Fate Zero", ];//"Sword Art Online", "Nanatsu no Taizai"];
 
-  updateButtons(topics);
-  function updateButtons(topics) {
-    var thead = $("<thead>").append($("<tr>").html("<td colspan='3'> <h2>Anime List</h2> </td>"));
-    $("table").append(thead);
+ 
 
-    var tbody = $("<tbody>");
-    var tableRow = $("<tr>");
-    for (var i = 0; i < topics.length; i++) {
+  updatePage(topics,"button");
+  
 
-      tableRow.append($("<td>").append("<button class='btn btn-info btn-sm'>" + topics[i] + "</button>"));
+  function updatePage(updateArr, btnORimg ) {
+    
+    var tbody;
+    var tableRow;
+    var noOfCols;
+    
+  
+    if (btnORimg == "button") 
+      {
+        noOfCols = 2;
+        tbody =$("#buttons-col");
+       $("#buttons-head").html("<tr> <td colspan='" + noOfCols + "'> <h2>Anime List</h2> </td> </tr>");
+  
+        
+      }
+
+    else 
+      {
+        noOfCols = 5;
+        tbody =$("#gifs-col");
+        $("#gifs-head").html("<tr> <td colspan='" + noOfCols + "'> <h2>Images </h2> </td> </tr>");
+        
+      }
+
+      tableRow =$("<tr>");
+  
+    for (var i in updateArr) {
+      
+      if (btnORimg == "button")
+      {
+        tableRow.append($("<td>").append("<button class='btn btn-info btn-sm'>" + updateArr[i] + "</button>"));
+      }
+      else
+      {
+        var tableCol = $("<td>")
+        tableCol.append("<h6> Rating : " + updateArr[i].gif_rating + "</h6>");
+        tableCol.append("<h6> Rating : " + updateArr[i].width + "</h6>");
+        tableCol.append("<h6> Rating : " + updateArr[i].height + "</h6>");
+        tableCol.append("<img src='" + updateArr[i].gif_img + "' />");
+        tableRow.append(tableCol);
+      }
+
+      tbody.append(tableRow);
       // Creating and storing a div tag
-
-      if ((i + 1) % 3 == 0) {
-        tbody.append(tableRow);
+      if (((parseInt(i) + 1) % noOfCols) == 0) {
         tableRow = $("<tr>");
       }
-      else if ((i + 1) == topics.length) {
-        tableRow.append($("<td>").append(" "));
-        tableRow.append($("<td>").append(" "));
-        tbody.append(tableRow);
-      }
     }
-    $("table").append(tbody);
   }
   // Adding click event listen listener to all buttons
   $("button").on("click", function () {
@@ -34,7 +64,7 @@ $(document).ready(function () {
     var anime = $(this).html();
 
     // Constructing a queryURL using the animal name
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + anime + "&api_key=dc6zaTOxFJmzC&limit=12";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + anime + "&api_key=dc6zaTOxFJmzC&limit=10";
     console.log(queryURL);
 
     // Performing an AJAX request with the queryURL
@@ -44,18 +74,20 @@ $(document).ready(function () {
     })
 
       .then(function (response) {
+        $("#gifs-col").html("");
+        var gif_Arr = [];
         for (i in response.data) {
           console.log("    ");
           var rating = response.data[i].rating;
           var still_image = response.data[i].images.original_still.url;
           var gif_image = response.data[i].images.preview_webp.url;
-          $("#gifs-col").append($("<span>").text(rating));
-          // $("#gifs-col").append($("<img>", { src: still_image }));
-          $("#gifs-col").append($("<img>", { src: gif_image }));
-
+          var gif_width = response.data[i].images.preview_webp.width;
+          var gif_height = response.data[i].images.preview_webp.height;
+          gif_Arr.push ({gif_rating : rating, gif_img : gif_image, still_img : still_image, width: gif_width, height : gif_height });
+          
         console.log(rating, still_image, gif_image);
         }
-
+        updatePage(gif_Arr, "gifs");
       });
   });
 });
