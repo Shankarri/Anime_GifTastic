5aa47be7c9641f632ce3bd92d8c99f488df43c41
Shanki <shanki.ranju@gmail.com>
2019-02-07 23:41:18 -0600

    almost done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,111 +1,169 @@
 
 $(document).ready(function () {
 
-  var topics = ["Fairy Tail", "Inuyasha", "Death Note", "Fushigi Yugi", "Yu Yu Hakusho", "Fate Zero",
-    "Hunter X Hunter", "Bleach", "Full Metal Alchemist", "Fate Zero",];//"Sword Art Online", "Nanatsu no Taizai"];
-    var datalimit = 12;
-    $("#user-msg").hide();
-    $("#limit-msg").hide();
+  var topics = ["Fairy_Tail", "Inuyasha", "Death_Note", "Sword_Art_Online", "Bleach", "Fate_Stay_Night", "Full_Metal_Alchemist", "Hunter_X_Hunter"];
 
-    updatePage(topics, "button");
+  var currentAnime = "";
+  var datalimit = 10;
+  $("#user-msg").hide();
+  $("#limit-msg").hide();
 
-  function updatePage(updateArr, btnORimg) {
+  updatePage(topics, "#buttons-col");
+
+  function updatePage(updateArr, btnORimgCol) {
+
     var contentbody;
     var divCol;
     $("#user-msg").hide();
 
-    if (btnORimg == "button") {
-      contentbody = $("#buttons-col");
-    }
-
-    else {
-
-      contentbody = $("#gifs-col");
-    }
-
     for (var i in updateArr) {
 
-      if (btnORimg == "button") {
+      if (btnORimgCol == "#buttons-col") {
         divCol = $("<div>").addClass("col-md-6 col-sm-6 p-2");
-        divCol.append("<button class='btn btn-info btn-sm anime-btn'>" + updateArr[i] + "</button>");
-        contentbody.append(divCol);
+        divCol.append("<button class='btn btn-info btn-sm anime-btn'> " + (updateArr[i]).replace(/\_/g, ' ') + "</button>");
+        divCol.append("<button class='btn btn-danger ml-1 p-0 btn-sm close-btn' id= " +updateArr[i] +"> X </button>");
+        $(btnORimgCol).append(divCol);
       }
       else {
+        // { gif_rating, gif_image, still_image});
+        
         divCol = $("<div>").addClass("col-md-3 col-sm-4 col-xs-6 pb-2 gif-border");
-        divCol.append("<p> Rating : " + updateArr[i].gif_rating + "</p>");
-        // tableCol.append("<h6> Rating : " + updateArr[i].width + "</h6>");
-        // tableCol.append("<h6> Rating : " + updateArr[i].height + "</h6>");
-        divCol.append("<img src='" + updateArr[i].gif_img + "' />");
-        contentbody.append(divCol);
+
+        divCol.append("<p class='m-0'> Rating : " + updateArr[i].gif_rating + "</p>");
+        divCol.append("<p class='m-0'> Title : " + (updateArr[i].gif_title).replace("GIF","").trim() + "</p>");
+        divCol.append("<p class='m-0'> Mp4 Size : " + updateArr[i].gif_size + "</p>");
+
+        var image = $('<img>').attr({
+            "src": updateArr[i].still_image,
+            "data-still_src": updateArr[i].still_image,
+            "data-gif_src": updateArr[i].gif_image,
+            "data-animated": false
+          });
+        
+          //  console.log("img",image.date());
+        divCol.append(image);
+        $(btnORimgCol).append(divCol);
       }
     }
 
   }
-  $("#submit-btn").on("click", function () {
-    var userInput = $("#user_anime").val().trim();
 
-    var newAnime = [];
-    if(userInput != "")
-    {  
-      newAnime[0] = userInput;
-      updatePage(newAnime, "button");
-      $("#user_anime").val("");
+  $(document).on("click", "img", function () {
+
+    console.log("this.data()", $(this).data());
+
+    if ($(this).data().animated) {
+      $(this).attr("src", $(this).data().still_src);
+      $(this).data().animated = false;
     }
-    else
-    {
-      $("#user-msg").show();
-      setTimeout(function(){  $("#user-msg").hide(); }, 4000);
+    else {
+
+      $("img").each(function () {
+        $(this).attr("src", $(this).data().still_src);
+      });
+
+      $(this).attr("src", $(this).data().gif_src);
+      $(this).data().animated = true;
     }
-    // $("#user-msg").show();
+
   });
 
-  $(document).on("click", "button" , function() {
-    
-  // Adding click event listen listener to all buttons
-  // $("button").on("click", function () {
+  $(document).on("click", ".anime-btn", function () {
 
-    var anime = $(this).html();
+    currentAnime = $(this).html();
+    $("#anime_title").text(currentAnime);
+    // Constructing a queryURL using the anime name
+
+    ajaxCall(currentAnime.toLowerCase());
+  });
+
+  $(document).on("click", ".close-btn", function () {
+
+    topics.splice(topics.indexOf($(this).attr("id")),1);
+    $("#buttons-col").empty();
+
+    if(currentAnime.trim() == (($(this).attr("id")).replace(/\_/g, ' ')).trim())
+    {
+      $("#gifs-col").empty();
+    }
+   
+    updatePage(topics, "#buttons-col");
     
-    // Constructing a queryURL using the animal name
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + anime + "&api_key=dc6zaTOxFJmzC&limit=" + datalimit;
-    console.log(queryURL);
+  });
+
+  function ajaxCall(currentAnime) {
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + currentAnime
+      + "&api_key=gmutM231fWK6pMu04oKqHGDuB1U9GDei&limit=" + datalimit;
+   // console.log(queryURL);
 
     // Performing an AJAX request with the queryURL
     $.ajax({
       url: queryURL,
       method: "GET"
     })
-
       .then(function (response) {
         $("#gifs-col").html("");
         var gif_Arr = [];
-        for (i in response.data) {
+        // console.log(this);
+        $.each(response.data, function () {
+         // console.log(this);
+          gif_Arr.push({
+            gif_title: this.title,
+            gif_rating: this.rating,
+            still_image: this.images.fixed_height_small_still.url,
+            gif_image: this.images.original.webp,
+            gif_size: this.images.original.webp_size,
+            
+          });
           
-          var rating = response.data[i].rating;
-          var still_image = response.data[i].images.original_still.url;
-          var gif_image = response.data[i].images.preview_webp.url;
-          var gif_width = response.data[i].images.preview_webp.width;
-          var gif_height = response.data[i].images.preview_webp.height;
-          gif_Arr.push({ gif_rating: rating, gif_img: gif_image, still_img: still_image, width: gif_width, height: gif_height });
-
-          // console.log(rating, still_image, gif_image);
-        }
-        updatePage(gif_Arr, "gifs");
+        });
+        updatePage(gif_Arr, "#gifs-col");
       });
-  });
-  $("[type=number]").change(function(){
-    $("#limit-msg").hide();
-    var limitVal=$(this).val();
-    if(limitVal >=10 && limitVal<=20)
-    { 
-    datalimit = limitVal;
-    $("#limit-display").text(limitVal);
-    
+
+
+     var bgQueryURL = "https://api.unsplash.com/search/photos?page=1&query="+currentAnime
+       + "&client_id=1306976fd8ac9edfaf357ffc9c2407e3c761108d34cbcca10772eef78a09182b";
+
+     $.ajax({
+      url: bgQueryURL,
+      method: "GET"
+    })  .then(function (response) {
+      console.log(response);
+ 
+    });
+  }
+
+  // Adding click event listen listener to all buttons
+
+  $("#submit-btn").on("click", function () {
+    var userInput = $("#user_anime").val().trim();
+
+    var newAnime = [];
+    if (userInput != "") {
+      newAnime[0] = userInput;
+      topics.push(userInput);
+      updatePage(newAnime, "#buttons-col");
+      $("#user_anime").val("");
     }
-    else
-    {
+    else {
+      $("#user-msg").show();
+      setTimeout(function () { $("#user-msg").hide(); }, 4000);
+    }
+  });
+
+  $("[type=number]").change(function () {
+    $("#limit-msg").hide();
+    var limitVal = $(this).val();
+    if (limitVal >= 10 && limitVal <= 20) {
+      datalimit = limitVal;
+      $("#limit-display").text(limitVal);
+      if (currentAnime != "")
+        ajaxCall(currentAnime.toLowerCase());
+    }
+    else {
       $("#limit-msg").show();
-      setTimeout(function(){  $("#limit-msg").hide(); }, 4000);
+      setTimeout(function () { $("#limit-msg").hide(); }, 4000);
     }
   });
 });
